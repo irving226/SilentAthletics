47534228d5351979d0f3f9198a65c4b31457df80
Brian Irving <brianirv@buffalo.edu>
2021-03-08 20:14:57 -0700

    Finally made a (temp) page for my web app, PROGRESS

---
+++ SilentAthleticsWebSite/Associated Databases/dbo.Accounts.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Accounts] (
+    [Id]           INT            IDENTITY (1, 1) NOT NULL,
+    [UserName]     NVARCHAR (MAX) NOT NULL,
+    [FirstName]    NVARCHAR (MAX) NOT NULL,
+    [Neighborhood] NVARCHAR (MAX) NOT NULL,
+    [Email]        NVARCHAR (MAX) NOT NULL,
+    [DateOfBirth]  INT            NOT NULL,
+    CONSTRAINT [PK_Accounts] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ SilentAthleticsWebSite/SilentAthleticsWebSite/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SilentAthleticsWebSite.Models;
+
+namespace SilentAthleticsWebSite.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SilentAthleticsWebSite.Models.Accounts> Accounts { get; set; }
+    }
+}
